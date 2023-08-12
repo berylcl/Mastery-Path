73bd632a9230940912e0f1a053969bef5b52552b
berylcl <gakiiberyl@gmail.com>
2023-08-12 21:31:15 +0100

    Adding second challenge to milestone 2

--- Variables & data types/milestone2.c
+++ Variables & data types/milestone2.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 int main()
 {
+  //celsius tp F
   double tempCelsius;
-  printf("Enter degree in °C");
+  printf("Enter degrees in °C");
   scanf("%lf", &tempCelsius);
   double result;
   result = tempCelsius * 1.8 + 32;
-  printf("The temp in farenheit is %.2lf",result);
+  printf("The temp in Farenheit is %.2lf F \n",result);
+  //F to celsius
+  double tempFarenheit;
+  printf("Enter degree in F : ");
+  scanf("%lf", &tempFarenheit);
+  double result2;
+  result2 = (tempFarenheit - 32)/1.8;
+  printf("The temp in  °C is : %.2lf",result2);
   return 0;
 }
