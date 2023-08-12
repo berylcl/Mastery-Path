ccae180a0ad8b1a198313e5749ec3ba43a6e6311
berylcl <gakiiberyl@gmail.com>
2023-08-12 12:13:30 +0100

    Add calculator folder

---
+++ Calculator/first_calc.c
@@ -0,0 +1,11 @@
+/*A basic calc to print result of five mathematical operations*/
+#include <stdio.h>
+int main ()
+{
+  printf("The total of two numbers is : %d\n",3 + 2);
+  printf("The diff between 789 and 322 is :%d\n",789-322);
+  printf("Multiplying 23 by 23 is : %d\n",23*23);
+  printf("dividing 22 by 3 is :%d \n",23/3);
+  printf("The remainder of 44 by 3 is :%d",43%3);
+  return 0;
+}
