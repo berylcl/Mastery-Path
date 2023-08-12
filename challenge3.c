f1b4f8ed2149c51ceddde7ba918b67da2f10b296
berylcl <gakiiberyl@gmail.com>
2023-08-12 05:51:22 +0100

    add challenge1

---
+++ challenge3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+  printf("  * \n");
+  printf(" *** \n");
+  printf("*****");
+  printf("\n  *  \n *** \n*****");
+  return 0;
+}
---
+++ hello-world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  printf("FirstName: Bushra\n");
+  printf("LastName: Mahmood \n");
+  printf("Age: 24\n");
+  printf("First-Name : Bushra \nLast-Name: Mahmood \nAge: 25\n");
+  return 0;
+}
